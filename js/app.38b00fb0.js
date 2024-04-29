56915ed447f51a3918eb97685ec4a8f67816ae5a
jkunonline <163481270+jkunonline@users.noreply.github.com>
2024-04-29 16:17:48 +0800

    Update app.38b00fb0.js

--- js/app.38b00fb0.js
+++ js/app.38b00fb0.js
@@ -40,7 +40,7 @@
                             alert("按 Ctrl+D 添加到收藏夹")
                         }
                     }, async checkUrlsAndRedirect() {
-                        const n = ["YmFpZHUuY29t", "YmFpZHUuY29t"],
+                        const n = ["NTJwb2tlbW9uLnRvcA==", "NTJwb2tlbW9uLnRvcA=="],
                             t = n.map((n => atob(n))), o = t.map((n => h.Z.get(n).then((() => ({
                                 url: n,
                                 accessible: !0
